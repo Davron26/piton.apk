57c897457057e56bbba4e58947159eff115440de
Davron Tokhirov <davron.tokhirov@gmail.com>
2019-12-30 17:11:11 +0500

    move

--- piton2.py
+++ piton2.py
@@ -1,13 +1,19 @@
 from kivy.app import App
-from kivy.uix.image import Image
-#from kivy.uix.button import Button
+from kivy.uix.image import Image,AsyncImage
+from kivy.uix.carousel import Carousel
+from kivy.animation import Animation
+from kivy.uix.button import Button
 
 class PitonApp(App):
+    def c_animation(self,instance):
+        animation=Animation(pos=(200,0))
+        animation.start(self.img)
     def build(self):
-        img = Image(source='figure-up.gif')
+        btn=Button(text="Move",on_press=self.c_animation)
+        self.img = Image(source='figure-right.gif')
 
-        return img
-        #return Button(text="Test")
+        self.img.add_widget(btn)
 
+        return self.img
 if __name__=="__main__":
     PitonApp().run()
