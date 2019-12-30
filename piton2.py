9deb2f7d3f77e9704eb7a4abaa1d6d608750a659
Davron Tokhirov <davron.tokhirov@gmail.com>
2019-12-31 00:55:56 +0500

    background white and move more

--- piton2.py
+++ piton2.py
@@ -1,19 +1,45 @@
 from kivy.app import App
-from kivy.uix.image import Image,AsyncImage
-from kivy.uix.carousel import Carousel
+from kivy.uix.image import Image
 from kivy.animation import Animation
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+Window.clearcolor = (1, 1, 1, 1)
 
 class PitonApp(App):
-    def c_animation(self,instance):
-        animation=Animation(pos=(200,0))
+    def r_animation(self,instance):
+        animation=Animation(x=+390)
+        animation.start(self.img)
+    def d_animation(self,instance):
+        animation=Animation(y=+160)
+        animation.start(self.img)
+    def l_animation(self,instance):
+        animation=Animation(x=-390)
+        animation.start(self.img)
+    def u_animation(self,instance):
+        animation=Animation(y=+440)
         animation.start(self.img)
     def build(self):
-        btn=Button(text="Move",on_press=self.c_animation)
-        self.img = Image(source='figure-right.gif')
+        gl=GridLayout(rows=1)
+        bl=BoxLayout(orientation='vertical')
 
-        self.img.add_widget(btn)
+        btn_u = Button(text="Up", on_press=self.u_animation)
+        btn_d = Button(text="Down", on_press=self.d_animation)
+        btn_l = Button(text="Left", on_press=self.l_animation)
+        btn_r = Button(text="Right",on_press=self.r_animation)
 
-        return self.img
+        self.img = Image(source='figure-down.gif')
+
+        bl.add_widget(self.img)
+        bl.add_widget(gl)
+
+        gl.add_widget(btn_u)
+        gl.add_widget(btn_d)
+        gl.add_widget(btn_r)
+        gl.add_widget(btn_l)
+
+        return bl
 if __name__=="__main__":
     PitonApp().run()
