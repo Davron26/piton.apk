27d05e0687195ef9e1b42a1a55e6927c6fad87f9
Davron Tokhirov <davron.tokhirov@gmail.com>
2019-12-31 14:23:19 +0500

    controls better

--- piton2.py
+++ piton2.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
 
 Window.clearcolor = (1, 1, 1, 1)
 
@@ -13,7 +14,7 @@ class PitonApp(App):
         animation=Animation(x=+390)
         animation.start(self.img)
     def d_animation(self,instance):
-        animation=Animation(y=+160)
+        animation=Animation(y=+60)
         animation.start(self.img)
     def l_animation(self,instance):
         animation=Animation(x=-390)
@@ -22,23 +23,25 @@ class PitonApp(App):
         animation=Animation(y=+440)
         animation.start(self.img)
     def build(self):
-        gl=GridLayout(rows=1)
+        gl=GridLayout(rows=2,padding=[0,100,0,0])
         bl=BoxLayout(orientation='vertical')
 
-        btn_u = Button(text="Up", on_press=self.u_animation)
-        btn_d = Button(text="Down", on_press=self.d_animation)
-        btn_l = Button(text="Left", on_press=self.l_animation)
-        btn_r = Button(text="Right",on_press=self.r_animation)
+        btn_u = Button(text="^", on_press=self.u_animation)
+        btn_d = Button(text="v", on_press=self.d_animation)
+        btn_l = Button(text="<", on_press=self.l_animation)
+        btn_r = Button(text=">",on_press=self.r_animation)
 
         self.img = Image(source='figure-down.gif')
 
         bl.add_widget(self.img)
         bl.add_widget(gl)
 
+        gl.add_widget(Widget())
         gl.add_widget(btn_u)
+        gl.add_widget(Widget())
+        gl.add_widget(btn_l)
         gl.add_widget(btn_d)
         gl.add_widget(btn_r)
-        gl.add_widget(btn_l)
 
         return bl
 if __name__=="__main__":
